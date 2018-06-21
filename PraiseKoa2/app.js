6a6eeca181e5bf04694e680ba035abb6c056db54
zhongyucheng <zhongyucheng@xiangrikui.com>
2018-06-22 00:17:40 +0800

    koa使用axios请求PHP数据

--- PraiseKoa2/app.js
+++ PraiseKoa2/app.js
@@ -1,5 +1,5 @@
 const Koa = require('koa');
-const Request = require('request');
+const axios = require('axios');
 const Route = require('koa-route');
 const Static = require('koa-static');
 const views = require('koa-views');
@@ -16,42 +16,22 @@ app.use(Static(__dirname + '/public'));
 // 设置路由
 const main = async ctx => await ctx.render('index');
 const index = async ctx => await ctx.render('praise');
-const getphp = (ctx, next) => {
+const getphp = async (ctx, next) => {
     let url = 'http://127.0.0.1/JCYD_2pro_PHP/index.php';
     let data = { action: 'select' }
-
-    // // 因为回调函数是异步的,如果直接这样写,相当于没有给定响应！！
-    // Request.post({ url: url, formData: data }, function (error, response, body) {
-    //     if (error) {
-    //         console.log(error);
-    //         next();
-    //     }
-    //     console.log(body);
-    //     ctx.body = "body";
-    // });
-
+    // let data = ctx.Request
 
     // 注意，这里有一个异步操作！！需要不能用同步回调的方式写！！
-    Request.post({ url: url, formData: data }, async function (error, response, body) {
-        if (error) {
+    // 估计axios要配置formData形式的上传,PHP才能收到
+    ctx.body = await axios.post(url, data)
+        .then(function (response) {
+            console.log(response);
+            return response.data;
+        })
+        .catch(function (error) {
             console.log(error);
-            await next();
-        }
-        console.log(await ctx)
-    });
-
-    ctx.body = 123;
-
-    // // let data = ctx.Request
-    // let url = 'http://127.0.0.1/JCYD_2pro_PHP/index.php';
-    // axios.post(url, { action: 'select' })
-    //   .then(function (response) {
-    //     console.log(response.data);
-    //   })
-    //   .catch(function (error) {
-    //     console.log(error);
-    //     next();
-    //   });
+            return error
+        });
 }
 app.use(Route.get('/', main));
 app.use(Route.get('/index/index', index));
