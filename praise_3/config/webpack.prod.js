8bceff227089332977c782dd2fc00ffcf1f71e93
EasonMar <12054648@qq.com>
2018-07-23 22:38:07 +0800

    babel-loader join

--- praise_3/config/webpack.prod.js
+++ praise_3/config/webpack.prod.js
@@ -8,7 +8,7 @@ module.exports = {
             path.join(__dirname, '../src/public/javascripts/index.es6'),
             path.join(__dirname, '../src/public/javascripts/add.js')
         ],
-        tag:[
+        tag: [
             path.join(__dirname, '../src/public/javascripts/tag.es6')
         ]
     },
@@ -16,10 +16,24 @@ module.exports = {
         filename: 'public/javascripts/[name]-[hash:5].js',
         path: path.join(__dirname, '../build')
     },
-    plugin: [
+    plugins: [
         // 不明白为什么还要配置这货，前面不是已经解决环境变量的问题了吗
-        new webpack.DefinePlugin({'process.env.NODE_ENV':'"prod"'}),
+        new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"prod"' }),
         // 自动刷新浏览器
-        new LiveReloadPlugin({appendScriptTag:true})
-    ]
+        new LiveReloadPlugin({ appendScriptTag: true })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.es6$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            }
+        ]
+    }
 };
